815b4d64cef633adcfa84a409985a1e09339e76c
ellisraputri <ellisraputri@gmail.com>
2026-02-14 13:16:45 +0700

    feat: add web request

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,31 @@
-import { Image } from 'expo-image';
 import { useState } from 'react';
-import { Platform, View, StyleSheet, Button, TextInput } from 'react-native';
+import { Platform, View, StyleSheet, Button,Text, TextInput } from 'react-native';
 
 
 export default function HomeScreen() {
-  const [text, onChangeText] = useState("Enter URL");
-  
+  const [text, onChangeText] = useState("https://reactnative.dev/movies.json");
+  const [webData, setWebData] = useState("");
+  const request = new XMLHttpRequest();
+
+  function handlePressButtonAsync(){
+    request.onreadystatechange = e => {
+      if (request.readyState !== 4) {
+        return;
+      }
+
+      if (request.status === 200) {
+        console.log("success", + request.responseText);
+        setWebData("Status: "+ request.status + " "+ request.statusText + " "+ request.responseText + " " + request.response)
+      }
+      else {
+        console.warn("error");
+        setWebData("Error: "+ request.status + " "+ request.statusText + " "+ request.responseText + " " + request.response)
+      }
+    };
+    request.open("GET", text);
+    request.send();
+  }
+
   return (
     <View style={styles.containerColumn}>
       <View style={styles.containerRow}>
@@ -16,10 +36,11 @@ export default function HomeScreen() {
         />
 
         <Button 
-          title = "Click me"
-          onPress={() => alert("stop pressing my button!")}
+          title = "Go Request"
+          onPress={() => handlePressButtonAsync()}
           />
       </View>
+      <Text>{webData}</Text>
     </View>
   );
 }
