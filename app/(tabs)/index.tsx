cca98efabc53349474f357ef1a21210b08f893cc
ellisraputri <ellisraputri@gmail.com>
2026-02-14 12:55:02 +0700

    feat: init and add button

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,28 @@
+import { Image } from 'expo-image';
+import { Platform, View, StyleSheet, Button } from 'react-native';
+
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.containerColumn}>
+      <View style={styles.containerRow}>
+        <Button 
+          title = "Click me"
+          onPress={() => alert("stop pressing my button!")}
+          />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  containerColumn: {
+    flexDirection: "column",
+    justifyContent: "space-between",
+    padding: 40,
+  },
+  containerRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  }
+});
